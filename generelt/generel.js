df3e674bb1d12639cc317670afb30366c7509ade
Emma Munch <emmasofie.munch@gmail.com>
2025-02-16 22:59:33 +0100

    forside og generel

--- generelt/generel.js
+++ generelt/generel.js
@@ -1,1 +1,18 @@
 // BURGER MENU
+// Hent
+var searchIcon = document.getElementById("search-icon");
+var searchField = document.getElementById("search-field");
+
+// klik på forstørrelsesglasset
+searchIcon.addEventListener("click", function (event) {
+  event.stopPropagation();
+  searchField.classList.toggle("active");
+  searchField.focus();
+});
+
+// Skjuler søgefeltet hvis der klikkes et andet sted på skærmen
+document.addEventListener("click", function (event) {
+  if (!searchField.contains(event.target) && event.target !== searchIcon) {
+    searchField.classList.remove("active");
+  }
+});
